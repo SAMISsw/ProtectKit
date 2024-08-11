0ba5fd585c7482b27bc4743fc3cc1022aa4db0f7
ProtectKit <123129846+SAMISsw@users.noreply.github.com>
2024-08-11 10:21:21 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,23 +3,18 @@ import PackageDescription
 
 let package = Package(
     name: "ProtectKit",
-    platforms: [
-        .iOS(.v14)
-    ],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "ProtectKit",
-            targets: ["ProtectKit"]
-        ),
+            targets: ["ProtectKit"]),
     ],
     targets: [
         .target(
             name: "ProtectKit",
-            dependencies: []
-        ),
+            dependencies: []),
         .testTarget(
             name: "ProtectKitTests",
-            dependencies: ["ProtectKit"]
-        ),
+            dependencies: ["ProtectKit"]),
     ]
 )
