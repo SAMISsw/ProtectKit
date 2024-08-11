ff1de642dfbe9439a972bae4fe199a6ca88afd5c
ProtectKit <123129846+SAMISsw@users.noreply.github.com>
2024-08-11 09:53:20 -0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.3
+import PackageDescription
+
+let package = Package(
+    name: "ProtectKit",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "ProtectKit",
+            targets: ["ProtectKit"]),
+    ],
+    targets: [
+        .target(
+            name: "ProtectKit",
+            dependencies: []),
+        .testTarget(
+            name: "ProtectKitTests",
+            dependencies: ["ProtectKit"]),
+    ]
+)
